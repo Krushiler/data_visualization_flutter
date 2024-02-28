3351c8eacf0e8fec1ced0c88178f0bfedec74b1a
Lazarev Daniil <daniil.kn2004@gmail.com>
2024-02-29 00:37:42 +0800

    Set path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:data_visualization/core/app.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   runApp(const App());
 }
